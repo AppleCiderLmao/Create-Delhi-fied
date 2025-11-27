16c7203358a65eafd4d9c5a88066529dac2b110b
LiterallyAple <jazmanarya@gmail.com>
2025-11-27 19:58:13 +0530

    bro i realised i named it wrong and it stayed with 0.1.1

--- forge/src/main/java/net/literallyapple/create_delhified/forge/CreateDelhifiedForge.java
+++ forge/src/main/java/net/literallyapple/create_delhified/forge/CreateDelhifiedForge.java
@@ -2,18 +2,22 @@ package net.literallyapple.create_delhified.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.literallyapple.create_delhified.CreateDelhified;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CreateDelhified.MOD_ID)
-public class CreateDMRCForge {
-    public CreateDMRCForge() {
+public class CreateDelhifiedForge {
+    public CreateDelhifiedForge() {
         // register Architectury API with the modEventBus so it doesn't keep crying
         EventBuses.registerModEventBus(CreateDelhified.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         // registrate must be given the mod event bus on forge before registration
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         CreateDelhified.REGISTRATE.registerEventListeners(eventBus);
         CreateDelhified.init();
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT,
+                () -> CreateDelhifiedForgeClient::init);
     }
 }
